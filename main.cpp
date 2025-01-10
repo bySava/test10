b7373c608b10ca8eeb2c41d1af37ca034087e888
bySava <saveliy.lozovoy@mail.ru>
2025-01-11 00:43:35 +0700

    Fast pow main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+double my_pow(double base, unsigned int exponent) {
+     if (exponent == 0) {
+        return 1.0; // Возведение в степень 0
+    }
+    
+    double half = my_pow(base, exponent / 2); // Рекурсивный вызов для половинной степени
+    
+    if (exponent % 2 == 0) {
+        return half * half; // Если степень четная
+    } else {
+        return half * half * base; // Если степень нечетная
+    }
+}
